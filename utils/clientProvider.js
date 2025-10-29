c9afa51876b8926a60978058b99440a64d593fb6
AnitBishwas <anit.biswas@swissbeauty.in>
2025-10-29 11:08:39 +0530

    fetch logger reposition

--- utils/clientProvider.js
+++ utils/clientProvider.js
@@ -1,5 +1,6 @@
 import sessionHandler from "./sessionHandler.js";
 import shopify from "./shopify.js";
+import "./debugger/fetchLogger.js";
 
 /**
  * Fetches the offline session associated with a shop.
