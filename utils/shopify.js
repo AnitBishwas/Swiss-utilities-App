cdb8f38d39738639ce228b97c10e010079e307b9
AnitBishwas <anit.biswas@swissbeauty.in>
2025-10-29 11:42:30 +0530

    logger removed

--- utils/shopify.js
+++ utils/shopify.js
@@ -14,7 +14,7 @@ let shopify = shopifyApi({
   hostScheme: "https",
   apiVersion: process.env.SHOPIFY_API_VERSION,
   isEmbeddedApp: true,
-  logger: { level: isDev ? LogSeverity.Info : LogSeverity.Debug, httpRequests: true },
+  logger: { level: isDev ? LogSeverity.Info : LogSeverity.Error },
 });
 
 /*
